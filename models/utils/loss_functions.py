ba01b8d3d1ed9b2694330e441845c27cbc0ccb7a
aevns <7926825+aevns@users.noreply.github.com>
2022-12-18 16:54:03 -0800

    + label presence pred. & loss (to gaussian_nll)

--- models/utils/loss_functions.py
+++ models/utils/loss_functions.py
@@ -10,20 +10,28 @@ def gaussian_entropy(pred):
     return torch.sum(torch.log(torch.det(cov_mat))/2 + 2.8378770664093455, dim=(-1))
 
 def gaussian_nll(pred, x):
+
+    
     pose = pred['pose'][:,:,0:2]
     cov_idx = torch.tensor([[2,4],[4,3]])
     cov_mat = pred['pose'][:,:,cov_idx]
+    labeled = pred['pose'][:,:,5]
     
     gt_pose = x['pose'][:,:,0:2]
     if x['pose'].shape[-1] == 3:
         mask = (x['pose'][:,:,2] != 0)
+
+        label_loss = -torch.sum(torch.log(labeled[mask])) - torch.sum(torch.log((1 - labeled)[~mask]))
+        # for use with imprefect ground truth masking
+        #label_loss = -mask * torch.log(labeled / mask) - (1 - mask) * torch.log((1 - labeled) / (1 - mask))
     else:
         mask = 1
+        label_loss = 0
 
     dif = torch.reshape(gt_pose - pose, (pose.shape[0], pose.shape[1], pose.shape[2], 1))
     q = torch.matmul(torch.transpose(dif,-1,-2), torch.matmul(torch.inverse(cov_mat), dif))
     q = q.view(q.shape[0], q.shape[1])
-    return torch.sum(mask * (torch.log(torch.det(cov_mat)) + q)/2 + 1.8378770664093455, dim=(-1))
+    return torch.sum(mask * (torch.log(torch.det(cov_mat)) + q)/2 + 1.8378770664093455, dim=(-1)) + label_loss
 
 def heatmap_target_mse(pred, x):
     return 0.5 * torch.mean(nn.MSELoss(reduction='none')(pred['heatmap'], x['target']), dim=(-3,-2,-1))
